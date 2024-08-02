9a597b479332c53af6deeee5902018b24c6c2073
Vitória Santana <vitoriagcsantana@gmail.com>
2024-08-02 15:35:43 -0300

    rota dashboard somente para pessoas logadas

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,5 +1,8 @@
+import { GetServerSideProps } from 'next';
 import Head from 'next/head';
 import styles from './styles.module.css';
+
+import { getSession } from 'next-auth/react';
 
 export default function Dashboard(){
     return(
@@ -12,3 +15,21 @@ export default function Dashboard(){
         </div>
     );
 }
+
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+    const session = await getSession({req});
+
+    if(!session?.user){
+        //se não têm usuário vamos redirecionar para /
+        return{
+            redirect: {
+                destination: "/",
+                permanent: false,
+            },
+        };
+    }
+    
+    return{
+        props: {},
+    };
+};
